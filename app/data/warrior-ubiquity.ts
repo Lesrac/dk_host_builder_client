0c311bb0d4902d2491017b6beb8ec3ef6e5228de
Lesrac <daniel.frick88@gmail.com>
2016-09-16 15:20:45 +0200

    change Ubiquity representation

--- app/data/warrior-ubiquity.ts
+++ app/data/warrior-ubiquity.ts
@@ -1,8 +1,9 @@
 import {Realm} from "./realm";
 import {Warrior} from "./warrior";
-import {Kin} from "./kin";
+import {Ubiquity} from "./ubiquity";
 export class WarriorUbiquity {
     realm: Realm;
     warrior: Warrior;
-    kin: Kin;
+    ubiquity: Ubiquity;
+    ubiquity_amount: number;
 }
--- app/warrior/warrior-detail.component.ts
+++ app/warrior/warrior-detail.component.ts
@@ -4,7 +4,6 @@ import {DarklandsService} from "../services/darklands.service";
 import {Params, ActivatedRoute} from "@angular/router";
 import {Warrior} from "../data/warrior";
 import {Kin} from "../data/kin";
-import {Realm} from "../data/realm";
 import {WarriorUbiquity} from "../data/warrior-ubiquity";
 
 @Component({
@@ -18,6 +17,7 @@ export class WarriorDetailComponent implements OnInit {
     warrior_ubiquities: WarriorUbiquity[];
     kins: string = "";
     realms: string = "";
+    ubiquities: string = "";
 
     constructor(private route: ActivatedRoute,
                 private darklandsService: DarklandsService) {
@@ -56,13 +56,28 @@ export class WarriorDetailComponent implements OnInit {
                     console.log("Realm:");
                     console.log(realm);
                     console.log(this.warrior_ubiquities);
-                    this.warrior_ubiquities.forEach(this.buildRealm, this);
+                    this.warrior_ubiquities.forEach(this.buildRealms, this);
+                    this.warrior_ubiquities.forEach(this.buildUbiquities, this);
                 });
         });
     }
 
-    buildRealm(warrior_ubiquity: WarriorUbiquity): void {
-        this.realms += warrior_ubiquity.realm.name;
+    buildRealms(warrior_ubiquity: WarriorUbiquity): void {
+        if (!this.realms.toUpperCase().includes((warrior_ubiquity.realm.name.toUpperCase()))) {
+            this.realms += warrior_ubiquity.realm.name;
+        }
+    }
+
+    buildUbiquities(warrior_ubiquity: WarriorUbiquity, index: number, array: WarriorUbiquity[]): void {
+        this.ubiquities += warrior_ubiquity.ubiquity.name;
+        console.log("Index: " + index);
+        console.log(array);
+        if (warrior_ubiquity.ubiquity_amount > 1) {
+            this.ubiquities += " (" + warrior_ubiquity.ubiquity_amount + ")";
+        }
+        if (index < array.length - 1) {
+            this.ubiquities += " + ";
+        }
     }
 
     goBack(): void {
